790dd9ba18b7a92f798941386eb189dc6c558ea2
luis <luis.evo_8.10@hotmail.com>
2016-07-10 13:25:04 -0500

    reporteController

---
+++ api/controllers/ReporteController.js
@@ -0,0 +1,11 @@
+/**
+ * ReporteController
+ *
+ * @description :: Server-side logic for managing reportes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
